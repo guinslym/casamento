e7c6354207f1013c92471473f44c5bc3c22e15fc
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-07 03:51:04 -0400

    customizing form

--- convidar/forms.py
+++ convidar/forms.py
@@ -23,15 +23,18 @@ class ReservationForm(forms.Form):
         # delete empty choice for the type
         del self.fields["acceptation"].choices[0]
 
-        self.helper.layout = Layout(
-                    'acceptation',
-                    'firstname',
-                    'lastname',
-                    'telephone',
-                    'accompagnement',
-                    'notes',
-                    ButtonHolder(
-                            Submit('reservation', 'Reservation',
-                            css_class='btn-primary')
-                        )
+        self.helper.layout = layout.Layout(
+            layout.Fieldset(
+                _("Main data"),
+                layout.Field("acceptation"),
+                layout.Field("firstname", css_class="input-block-level"),
+                layout.Field("lastname", css_class="input-block-level"),
+                layout.Field("telephone", css_class="input-block-level"),
+                layout.Field("accompagnement", css_class="input-block-level"),
+                layout.Field("notes", css_class="input-block-level", rows="3"),
+            ),
+            ButtonHolder(
+                    Submit('reservation', 'Reservation',
+                    css_class='btn-primary')
                 )
+        )
