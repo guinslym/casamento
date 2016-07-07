4bfc30e845ae64bd22a6078f9da1987ccbe64a09
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-07 04:25:13 -0400

    troubleshooting templates

--- convidar/urls.py
+++ convidar/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     url(r'^create/$', 'convidar.views.ReservationCreateView', name="add_invitation"),
     url(r'^$',
-        TemplateView.as_view(template_name='partials/locstructure.html'),
+        TemplateView.as_view(template_name='hello.html'),
         name="add_invitation"),
 
 )
